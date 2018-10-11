9675536c188c2b3fb6a53e4c68987b64e73413c7
Andrew Mackarous <a.mackarous@gmail.com>
2018-10-11 14:48:53 -0400

    Initial Commit

---
+++ Torrential/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Torrential
+//
+//  Created by Andrew Mackarous on 2018-10-11.
+//  Copyright © 2018 Mackarous. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Torrential/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Torrential
+//
+//  Created by Andrew Mackarous on 2018-10-11.
+//  Copyright © 2018 Mackarous. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
