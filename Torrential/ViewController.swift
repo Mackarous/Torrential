aa21240a8d6f4d9f5cb13802c16c0183a3bd425c
Andrew Mackarous <a.mackarous@gmail.com>
2018-10-18 19:39:23 -0400

    Added UI for loading torrent info

--- Torrential/ViewController.swift
+++ Torrential/ViewController.swift
@@ -8,21 +8,29 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+final class ViewController: NSViewController {
+    @IBOutlet private var createdByField: NSTextField!
+    @IBOutlet private var creationDateField: NSTextField!
+    @IBOutlet private var trackerURLField: NSTextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        view.registerForDraggedTypes([.fileURL])
-    }
-    
     @IBAction func openFile(_ sender: Any) {
         let panel = NSOpenPanel()
         panel.allowedFileTypes = ["torrent"]
         let clicked = panel.runModal()
         guard clicked == .OK, let url = panel.url else { return }
         let reader = TorrentReader(fileURL: url)
-        let torrentData = reader.loadTorrentMetadata()
-        print(torrentData)
+        let metadata = reader.loadTorrentMetadata()
+        createdByField.stringValue = metadata?.createdBy ?? "Unkown"
+        creationDateField.stringValue = metadata?.creationDate?.stringValue ?? "Unknown"
+        trackerURLField.stringValue = metadata?.announce ?? "Unknown"
     }
 }
 
+extension Date {
+    var stringValue: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        formatter.timeStyle = .long
+        return formatter.string(from: self)
+    }
+}
